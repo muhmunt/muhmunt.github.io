6dd4280c34573959d1235490c7d46c04adbbb27b
agam_yesdok <m.muntasir@yesdok.com>
2022-12-12 18:12:07 +0700

    updated personal website

---
+++ js/command.js
@@ -0,0 +1,171 @@
+var password = "backend engineer";
+var linkedin = "https://www.linkedin.com/in/muhammad-muntasir-138a4119a/";
+var instagram = "https://www.instagram.com/agam.am/";
+var github = "https://github.com/muhmunt/";
+var email = 'mailto:muhmunt@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Muhammad Muntasir!>‹",
+  "Hi My Name is Muhammad Muntasir, I Was Self Employed",
+  "<u>Backend Developer</u> , Lets take a look Some of My Personal Information and Experiences in 2 years",
+  "<br>",
+  "Personal Info : ",
+  "Name: Muhammad Muntasir",
+  "Date of Birth: 01-10-2002",
+  "Email: muhmunt@gmail.com",
+  "Phone: +62 822-1737-6369",
+  "<br>",
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+whoami = [
+  "<br>",
+  "The paradox of â€œWho am I?â€ is: we never know, but, we constantly find out.",
+  "<br>",
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/muhammadmuntasir' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/agam.am' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/muhmunt' + "</a>",
+  "<br>",
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+experiences = [
+  "<br>",
+  '<span class="command">(*)Back End Developer YESDOK  June 2022 - Present </span>',
+  '<ul>',
+  '  <li>Developing and provide an API for application</li>',
+  '  <li>Communicating with the team in the process of build an feature</li>',
+  '  <li>Using an effective and reusable code</li>',
+  '  <li>Fixing Bugs an existing code on Production & Staging</li>',
+  '  <li>Developing an Socket for a realtime data needed</li>',
+  '  <li>Implementing from research about binary search in existing feature search with',
+  '    javascript language</li>',
+  '  <li>Developing a queue job for downloading files</li>',
+  '  <li>Developing blast email for all doctor in databases</li>',
+  '</ul>',
+  "<br>",
+  '<span class="command">(*)Back End Developer DDTC December 2021 - June 2022</span>',
+  '<ul>',
+  '  <li>Developing & Bug Fix Legacy Application (Laravel)</li>',
+  '  <li>DDTC News, DDTC Academy, Perpajakan v1 and Internal Application.</li>',
+  '  <li>Using an effective and reusable code</li>',
+  '  <li>Fixing Bugs an existing code on Production & Staging</li>',
+  '  <li>Developing an Royalty feature at News Website.</li>',
+  '  <li>Communicate with Product Owner about the next feature will develop.',
+  '  </li>',
+  '  <li>Grooming Feature in legacy application</li>',
+  '</ul>',
+  "<br>",
+  '<span class="command">(*)Back End Developer & Web Developer , PT. Sembilan Sembilan Media Dec 2020 - Dec 2021 </span>',
+  '<ul>',
+  '  <li>KBM (Komunitas Bisa Menulis) APP (Microservice API Node Js)</li>',
+  '  <li>KBM (Komunitas Bisa Menulis) Website (ReactJs)</li>',
+  '  <li>KBM (Komunitas Bisa Menulis) Website (Laravel) (https://kbm.id)</li>',
+  '  <li>I and my team was challanged to build Digital Book Platform that focus on transaction',
+  '    on story chapter using wallet coin, It was very pleasing experiences working on such a',
+  '    big project, I am writting "case study, experiences and problem solving situation"',
+  '    bellow on the Case of Study sections.</li>',
+  '</ul>',
+  "<br>",
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+studycase = [
+  '(*)Provide Authentication API Based on JWT (JSON Web Token).',
+  '(*)Developing Microservice Node Js API',
+  '(*)Integration API Midtrans & Xendit payment',
+  '(*)Sketch SPA Website React Js from Design Figma',
+  '(*)Fetching Data and Lazy Loading digital book E-commerce transaction Product.',
+  '(*)Developing Daily Mission API',
+  '(*)Developing Earnings of user',
+  '(*)Building Progressive WebApps, Design Offline Storage, and Build HTML5/JS API',
+  'Feature.',
+  '(*)Developing Transaction of merchant from product partner',
+  '(*)Developing Timeline benchmark to facebook post',
+  '(*)Developing Commission affiliate of the month',
+  '(*)Developing Single Page Application (SPA), Routing Client Side UI Component,',
+  'Rendering SVG, DOM or VDOM (React)',
+  '(*)Designing model digital book platform',
+  '(*)Developing Pin e-wallet',
+  '(*)Developing of Shopping-cart Back-end.',
+  '(*)Developing Product branch multiple partner',
+  '(*)Absent QR Code based on website',
+  '(*)Developing React Js graphic of covid 19',
+  '(*)Developing React Js weather information PWA',
+  '(*)Case of study reporting problem in public on website',
+  '(*)Develop Booking and Reservations Management System.',
+  '(*)Creating Withdraw Balance of Sales.',
+  '(*)Designing Content Home Rotation',
+  '(*)Developing Website Digital Book platform',
+  '(*)Developing business model coin e-wallet on app',
+  '(*)Developing with redis',
+  '(*)Developing an Application POS API for Delivery food application in UEA',
+'',
+  '(*)Developing API with Laravel & Slim',
+  '(*)Developing API with Lumen',
+  '(*)Developing API with Golang',
+  '(*)Developing Socket for a realtime data needed',
+  '(*)Implementing Clean Code & Design Pattern',
+  '(*)Debug & Fixing in a Production mode',
+  '(*)Communicating with several division (Product, UIUX, IOS, Android) to spill',
+  'the tea',
+  '(*)Developing using NoSql (MongoDB, Redis)',
+  '(*)Developing using MySql',
+  '(*)Developing a queue job for downloading files and make for list download',
+  'history',
+  '(*)Developing blast email for all doctor in database',
+  '<br>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+profile = [
+  '<br>',
+  '<img style="width: 45%;height: auto;" class="mx-auto text-center" src="./img/content/cv_bw.jpeg" alt=""></img>',
+  '<br>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>"
+]
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>                     Who is Forrest?',
+  '<span class="command">whoami</span>                    Who are you?',
+  '<span class="command">social</span>                    Display social networks',
+  '<span class="command">profile</span>                   Display profile',
+  '<span class="command">secret</span>                    Find the password',
+  '<span class="command">experiences</span>               View experiences',
+  '<span class="command">studycase</span>    View study case experiences',
+  '<span class="command">history</span>                   View command history',
+  '<span class="command">help</span>                      You obviously already know what this does',
+  '<span class="command">email</span>                     Do not email me',
+  '<span class="command">clear</span>                     Clear terminal',
+  '<span class="command">banner</span>                    Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">MuhammadMuntasir (Terminal) Not A Commander. All knights reserved.</span>',
+  "/$$      /$$                       /$$                         /$$            ",
+  "| $$$    /$$$                      | $$                        |__/           ",
+  "| $$$$  /$$$$ /$$   /$$ /$$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$$ /$$  /$$$$$$  ",
+  "| $$ $$/$$ $$| $$  | $$| $$__  $$|_  $$_/   |____  $$ /$$_____/| $$ /$$__  $$ ",
+  "| $$  $$$| $$| $$  | $$| $$  \ $$  | $$      /$$$$$$$|  $$$$$$ | $$| $$  \__/ ",
+  "| $$\  $ | $$| $$  | $$| $$  | $$  | $$ /$$ /$$__  $$ \____  $$| $$| $$       ",
+  "| $$ \/  | $$|  $$$$$$/| $$  | $$  |  $$$$/|  $$$$$$$ /$$$$$$$/| $$| $$       ",
+  "|__/     |__/ \______/ |__/  |__/   \___/   \_______/|_______/ |__/|__/       ",
+                                                                          
+  '<i class="color2"><span>"Lets</span>  discuss with me I can help you in the field of battle and start fight the <span style="border-bottom: solid 3px #4a5568">world.</span>"</i>',
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
